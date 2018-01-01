80387e819b3bb79e5751f285ba2ab528bd9337c5
horkerps <horkerps@users.noreply.github.com>
2018-01-02 05:55:03 +0900

    Changed OxyPlot dlls setup

--- .build.ps1
+++ .build.ps1
@@ -1,7 +1,17 @@
 
-task . Load, Build, LocalImport
+task . LoadDevModule, Build, LocalImport
 
-task Load {
+task SetupOxyPlot {
+  Install-Package OxyPlot.Core -Destination lib
+  Install-Package OxyPlot.Wpf -Destination lib
+
+  $null = mkdir "$PSScriptRoot\OxyPlotCli\lib" -force
+
+  (Get-Item "$PSScriptRoot\lib\OxyPlot.Core.*\lib\net40\OxyPlot.dll").CopyTo("$PSScriptRoot\OxyPlotCli\lib\OxyPlot.dll")
+  (Get-Item "$PSScriptRoot\lib\OxyPlot.Wpf.*\lib\net40\OxyPlot.Wpf.dll").CopyTo("$PSScriptRoot\OxyPlotCli\lib\OxyPlot.Wpf.dll")
+}
+
+task LoadDevModule {
   Import-Module HorkerTemplateEngine
   OxyPlotCli\AssemblyLoader.ps1
 }
--- scripts/AssemblyLoader.ps1
+++ scripts/AssemblyLoader.ps1
@@ -1,3 +1,2 @@
-
-Add-Type -Path (Join-Path $PSScriptRoot "OxyPlot.Core.1.0.0\lib\net40\OxyPlot.dll")
-Add-Type -Path (Join-Path $PSScriptRoot "OxyPlot.Wpf.1.0.0\lib\net40\OxyPlot.Wpf.dll")
+Add-Type -Path "$PSScriptRoot\lib\OxyPlot.dll"
+Add-Type -Path "$PSScriptRoot\lib\OxyPlot.Wpf.dll"
