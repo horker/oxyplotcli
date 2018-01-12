5e276334ab9a254a4613aaee23035a3b96bea1dc
horkerps <horkerps@users.noreply.github.com>
2018-01-13 00:59:38 +0900

    Update New-OxyPlotModel to accept pipeline parameter

--- templates/PlotModel.template.ps1
+++ templates/PlotModel.template.ps1
@@ -3,13 +3,25 @@ Set-StrictMode -Version 3
 function New-OxyPlotModel {
   [cmdletbinding()]
   param(
+    [Parameter(ValueFromPipeline=$true, Mandatory=$false)]
+    [OxyPlot.Series.Series]$InputObject,
     [OxyPlot.Series.Series[]]$Series,
     [string]$StyleName,
 <% ..\tools\Insert-PropertyList.ps1 -OutputType "param" -ClassName "OxyPlot.PlotModel" -Indent 4 -%>
     [hashtable]$Option = @{}
   )
 
+begin {
   $model = New-Object OxyPlot.PlotModel
+}
+
+process {
+  if ($null -ne $InputObject) {
+    $model.Series.Add($InputObject)
+  }
+}
+
+end {
   foreach ($s in $Series) {
     $model.Series.Add($s)
   }
@@ -19,6 +31,7 @@ function New-OxyPlotModel {
 <% ..\tools\Insert-PropertyList.ps1 -OutputType "assign" -ClassName "OxyPlot.PlotModel" -VariableName "model" -OptionHashName "Option" -Indent 2 -%>
 
   $model
+}
 }
 
 function Update-OxyPlotModel {
