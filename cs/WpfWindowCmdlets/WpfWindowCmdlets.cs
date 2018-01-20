04784a7346d416661eb96a74445b31d7c6f2969b
horkerps <horkerps@users.noreply.github.com>
2018-01-20 22:36:04 +0900

    Make the internal root window invisible in the task switcher

--- cs/WpfWindowCmdlets/WpfWindowCmdlets.cs
+++ cs/WpfWindowCmdlets/WpfWindowCmdlets.cs
@@ -11,9 +11,82 @@ using System.Windows;
 using System.Windows.Markup;
 using System.Windows.Media;
 using System.Reflection;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
 
 namespace WpfWindowCmdlets
 {
+    // Win32 API functions and constants To make a window invisible in the task switcher
+    // ref.
+    // https://social.msdn.microsoft.com/Forums/en-US/9c4ada92-5065-4abb-a295-d62e5ddaf2b1/wpf-window-is-showen-in-alttab-list-though-windowstylenone-showintaskbarfalse?forum=wpf
+
+    class Win32Api
+    {
+        [Flags]
+        public enum ExtendedWindowStyles
+        {
+            WS_EX_TOOLWINDOW = 0x00000080
+        }
+
+        public enum GetWindowLongFields
+        {
+            GWL_EXSTYLE = (-20)
+        }
+
+        [DllImport("kernel32.dll", EntryPoint = "SetLastError")]
+        public static extern void SetLastError(int dwErrorCode);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", SetLastError = true)]
+        private static extern IntPtr IntSetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong", SetLastError = true)]
+        private static extern Int32 IntSetWindowLong(IntPtr hWnd, int nIndex, Int32 dwNewLong);
+
+        public static IntPtr SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong)
+        {
+            int error = 0;
+            IntPtr result = IntPtr.Zero;
+            // Win32 SetWindowLong doesn't clear error on success
+            SetLastError(0);
+
+            if (IntPtr.Size == 4) {
+                // use SetWindowLong
+                Int32 tempResult = IntSetWindowLong(hWnd, nIndex, IntPtrToInt32(dwNewLong));
+                error = Marshal.GetLastWin32Error();
+                result = new IntPtr(tempResult);
+            }
+            else {
+                // use SetWindowLongPtr
+                result = IntSetWindowLongPtr(hWnd, nIndex, dwNewLong);
+                error = Marshal.GetLastWin32Error();
+            }
+
+            if ((result == IntPtr.Zero) && (error != 0)) {
+                throw new System.ComponentModel.Win32Exception(error);
+            }
+
+            return result;
+        }
+
+        private static int IntPtrToInt32(IntPtr intPtr)
+        {
+            return unchecked((int)intPtr.ToInt64());
+        }
+
+        public static void MakeWindowInvisibleInTaskSwitcher(Window window)
+        {
+            WindowInteropHelper wndHelper = new WindowInteropHelper(window);
+
+            int exStyle = (int)GetWindowLong(wndHelper.Handle, (int)GetWindowLongFields.GWL_EXSTYLE);
+
+            exStyle |= (int)ExtendedWindowStyles.WS_EX_TOOLWINDOW;
+            SetWindowLong(wndHelper.Handle, (int)GetWindowLongFields.GWL_EXSTYLE, (IntPtr)exStyle);
+        }
+    }
+
     public class Util
     {
         static public bool IsWindowClosed(Window w)
@@ -28,10 +101,12 @@ namespace WpfWindowCmdlets
             window.AllowsTransparency = true;
             window.Background = Brushes.Transparent;
             window.WindowStyle = WindowStyle.None;
-            window.Width = 0;
-            window.Height = 0;
             window.ResizeMode = ResizeMode.NoResize;
             window.ShowInTaskbar = false;
+
+            window.Loaded += (sender, args) => {
+                Win32Api.MakeWindowInvisibleInTaskSwitcher((Window)sender);
+            };
 
             result.Add(window);
 
